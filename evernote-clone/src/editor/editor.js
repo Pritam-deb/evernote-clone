e4e5440330a3b5e092b978f95ec81b4c49c0bf0b
Pritam <pritamdebnath297@gmail.com>
2021-04-21 12:26:16 +0530

    feat : utilized debounce func
    updating database realtime after 1.5secs

--- evernote-clone/src/editor/editor.js
+++ evernote-clone/src/editor/editor.js
@@ -1,16 +1,39 @@
 
-import React from 'react';
-// import ReactQuill from 'react-quill';
-// import debounce from '../helpers';
-// import BorderColorIcon from '@material-ui/icons/BorderColor';
-// import { withStyles } from '@material-ui/core/styles';
-// import styles from './styles';
-// 
-function EditorComponent() {
+import React, {useState, useRef} from 'react';
+import ReactQuill from 'react-quill';
+import debounce from '../helpers';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+function EditorComponent(props) {
+      const [text, setText] = useState('');
+      const [title, setTitle] = useState('');
+      const [id, setId] = useState('');
+      const classes = props;
+
+
+      const updateBody = async(val)=>{
+            await setText(val);
+            update();
+      };
+
+      const update = useRef(
+		debounce(() => {
+			console.log('updating database!')
+		}, 1500)
+	).current
+
 
     return (
         
-            <h3>hello from editor</h3>
+            <div className={classes.editorContainer}>
+                  <ReactQuill
+                        value = {text}
+                        onChange={updateBody}>
+
+                  </ReactQuill>
+            </div>
       );
 }
 
