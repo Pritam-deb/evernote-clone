fa859bfed02ce75889c069aa29be4be1f88cefbc
Pritam <pritamdebnath297@gmail.com>
2021-04-22 18:35:50 +0530

    feat : added cancel button

--- evernote-clone/src/sidebar/sidebar.js
+++ evernote-clone/src/sidebar/sidebar.js
@@ -5,15 +5,21 @@ import List from '@material-ui/core/List';
 import { Divider, Button } from '@material-ui/core';
 import SidebarItemComponent from '../sidebaritem/sidebarItem';
 
-function SidebarComponent(props) {
+function SidebarComponent(props)
+{
       const [addingNote, setAddingNote] = useState(false);
       const [title, setTitle] = useState(null);
 
       const newNoteBtnClick = () => {
-            console.log('New button clicked');
+            setAddingNote (!(addingNote));
+            setTitle (null);
       }
 
       const { notes, classes, selectedNoteIndex } = props;
+
+      const updateTitle = (txt) => {
+            console.log('here it is: ',txt);
+      }
 
       
 
@@ -21,7 +27,18 @@ function SidebarComponent(props) {
             <div className={classes.sidebarContainer}>
                   <Button 
                         onClick={newNoteBtnClick}
-                        className={classes.newNoteBtn}>New Note</Button>
+                        className={classes.newNoteBtn}>{addingNote ? 'Cancel' : 'New Note'}</Button>
+                  {
+                        addingNote ? 
+                        <div>
+                              <input type = 'text'
+                              className = {classes.newNoteInput}
+                              placeholder = 'Enter note title'
+                              onKeyUp={(e)=>updateTitle(e.target.value)}>
+                              </input>
+                        </div> : 
+                              null
+                  }
             </div>
       );
 }
